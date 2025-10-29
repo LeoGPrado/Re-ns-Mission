3dc19b99fc469f8c05647cbe6425e7c9f320de07
Hecateo <juniorcubamontero83@gmail.com>
2025-10-29 15:45:40 -0500

    selector de armas
    selector de armas funcionando

--- Assets/scripts/Scripts/Pantallas/MenuDeSeleccionArmas/ControlDeSeleccion.cs
+++ Assets/scripts/Scripts/Pantallas/MenuDeSeleccionArmas/ControlDeSeleccion.cs
@@ -13,47 +13,60 @@ public class ControlDeSeleccion : MonoBehaviour
     [SerializeField] GameObject Mazo;
     [SerializeField] GameObject pincel;
 
+    private void Start()
+    {
+        Time.timeScale = 0f;
+    }
     public void apagarSeleccion()
     {
         canvaSeleciionDeObjeto.SetActive(false);
+        Time.timeScale = 1f;
     }
 
     public void ElegirEspada()
     {
         Espada.SetActive(true);
+        Time.timeScale = 1f;
     }
 
     public void ElegirArco()
     {
         Arco.SetActive(true);
+        Time.timeScale = 1f;
     }
 
     public void ElegirPollo()
     {
         Pollo.SetActive(true);
+        Time.timeScale = 1f;
     }
 
     public void ElegirBaston()
     {
         Baston.SetActive(true);
+        Time.timeScale = 1f;
     }
 
     public void ElegirCuchilla()
     {
         Cuchilla.SetActive(true);
+        Time.timeScale = 1f;
     }
 
     public void ElegirPescado()
     {
         Pescado.SetActive(true);
+        Time.timeScale = 1f;
     }
     public void ElegirMazo()
     {
         Mazo.SetActive(true);
+        Time.timeScale = 1f;
     }
 
     public void ElegirPincel()
     {
         pincel.SetActive(true);
+        Time.timeScale = 1f;
     }
 }

812297cbe982e7517a5574b375ac20e2b05bd86b
Hecateo <juniorcubamontero83@gmail.com>
2025-10-28 19:32:27 -0500

    avanze
    avanze

--- Assets/scripts/Scripts/personajes/ren/InteraccionEnemigos/cuerpoACuerpo/DestruirV2.cs
+++ Assets/scripts/Scripts/personajes/ren/InteraccionEnemigos/cuerpoACuerpo/DestruirV2.cs
@@ -2,18 +2,25 @@ using UnityEngine;
 
 public class DestruirV2 : MonoBehaviour
 {
-    private void Start()
+    private bool puedeDestruir = false;
+
+    public void ActivarDestruccion()
     {
-        enabled = false; 
+        puedeDestruir = true;
+    }
+
+    public void DesactivarDestruccion()
+    {
+        puedeDestruir = false;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (!puedeDestruir) return;
 
-        if (collision.gameObject.tag == "Enemigo")
+        if (collision.gameObject.CompareTag("Enemigo"))
         {
-
-            //Destroy(collision.gameObject);        
+            Destroy(collision.gameObject);
         }
     }
 }

e04cfd024ddb4b4bb8feedceff37768a3cfad78e
Hecateo <juniorcubamontero83@gmail.com>
2025-10-28 19:20:47 -0500

    asd
    asfafasd

--- Assets/scripts/Scripts/personajes/ren/InteraccionEnemigos/cuerpoACuerpo/DestruirV2.cs
+++ Assets/scripts/Scripts/personajes/ren/InteraccionEnemigos/cuerpoACuerpo/DestruirV2.cs
@@ -2,8 +2,14 @@ using UnityEngine;
 
 public class DestruirV2 : MonoBehaviour
 {
+    private void Start()
+    {
+        enabled = false; 
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
+
         if (collision.gameObject.tag == "Enemigo")
         {
 

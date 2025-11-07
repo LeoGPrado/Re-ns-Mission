415f31e75b47386a11b02e076b56a7d9a420a7e6
hernando cuba <juniorcubamontero83@gmail.com>
2025-11-07 13:17:51 -0500

    Ajustes Pre-presentación

--- Assets/scripts/Scripts/UI/HUD Gameplay/PuertaHP.cs
+++ Assets/scripts/Scripts/UI/HUD Gameplay/PuertaHP.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 public class PuertaHP : MonoBehaviour
 {
     public RectTransform HealthUI;
-    private float HealthSize = 700f;
+    private float HealthSize = 647.5f;
     public static PuertaHP BarraVida;
     private void Awake()
     {
@@ -37,7 +37,7 @@ public class PuertaHP : MonoBehaviour
     public void BajarHP()
     {
         HealthSize -= 50;
-        HealthUI.sizeDelta = new Vector2(HealthSize, 50);
+        HealthUI.sizeDelta = new Vector2(HealthSize, 4.945001f);
         if (HealthSize < 0)
         {
             SceneManager.LoadScene("FinDemo");

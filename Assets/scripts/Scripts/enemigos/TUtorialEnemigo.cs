9c3618080fb6e7cf3526025928842cc3e8a3200c
Hecateo <juniorcubamontero83@gmail.com>
2025-11-12 21:12:53 -0500

    avance goblin
    se avanzo la animacion

---
+++ Assets/scripts/Scripts/enemigos/TUtorialEnemigo.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class TUtorialEnemigo : MonoBehaviour
+{
+    public GameObject goblinTutorial;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ApareceGoblin()
+    {
+        goblinTutorial.SetActive(true);
+    }
+}

d5fc2ce71b51b77e82ee595eb646b5c3ad69d077
hernando cuba <juniorcubamontero83@gmail.com>
2025-11-28 13:11:26 -0500

    avance
    avance de bala Slime

---
+++ Assets/scripts/Scripts/enemigos/Slime/InvocacionDeBalaSlime.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class InvocacionDeBalaSlime : MonoBehaviour
+{
+    public GameObject balaNormal;
+    public Transform puntoAparicion;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "protegonista")
+        {
+            GameObject BalaSlime = Instantiate(balaNormal, puntoAparicion.position, puntoAparicion.rotation);
+        }
+    }
+}

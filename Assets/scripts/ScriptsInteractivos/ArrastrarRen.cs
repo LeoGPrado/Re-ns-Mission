e7ca41dfa698d33c1b3c56012a9d3a7ddec7bf08
Hecateo <juniorcubamontero83@gmail.com>
2025-11-29 19:31:38 -0500

    añadio
    se añadio la escena interactiva

---
+++ Assets/scripts/ScriptsInteractivos/ArrastrarRen.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class ArrastrarRen : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+
+    public bool Entro = false;
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "PasarCiudad")
+        {
+            Entro = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "PasarCiudad")
+        {
+            Entro = false;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (Entro==true)
+        {
+            SceneManager.LoadScene("prueba mapa 1");
+        }
+    }
+}
